87d82b3dd3b7ae02e2c2fbcb56f61ccc1bc9e8b2
1zuu <isurualagiyawanna9717@gmail.com>
2021-08-26 08:47:42 +0530

    update app.run

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def predictions():
         print(e)
 
 if __name__ == '__main__':
-    app.run(debug=True, host=heroku_host, port=heroku_port, threaded=False, use_reloader=False)
+    app.run(debug=True, host=heroku_url, port=heroku_port, threaded=False, use_reloader=False)
 
 
 # {"description" : "In the highly anticipated Thinking, Fast and Slow, Kahneman takes us on a groundbreaking tour of the mind and explains the two systems that drive the way we think. System 1 is fast, intuitive, and emotional; System 2 is slower, more deliberative, and more logical. Kahneman exposes the extraordinary capabilities—and also the faults and biases—of fast thinking, and reveals the pervasive influence of intuitive impressions on our thoughts and behavior. The impact of loss aversion and overconfidence on corporate strategies, the difficulties of predicting what will make us happy in the future, the challenges of properly framing risks at work and at home, the profound effect of cognitive biases on everything from playing the stock market to planning the next vacation—each of these can be understood only by knowing how the two systems work together to shape our judgments and decisions."}   
